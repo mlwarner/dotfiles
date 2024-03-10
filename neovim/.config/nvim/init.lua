587b1b05dc5212bcce03e17c1bcc04c69c3fbbf8
Matt Warner <mlwarner632@pm.me>
2024-03-10 00:54:21 -0800

    Document cmp actions

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -581,26 +581,37 @@ require("luasnip.loaders.from_vscode").lazy_load()
 luasnip.config.setup()
 
 cmp.setup({
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'path' },
-    { name = 'codeium' },
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
   },
+  completion = { completeopt = 'menu,menuone,noinsert' },
   mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    -- Select the [n]ext item
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    -- Select the [p]revious item
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+    -- Accept ([y]es) the completion.
+    --  This will auto-import if your LSP supports it.
+    --  This will expand snippets if the LSP sent a snippet.
+    ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+    -- Manually trigger a completion from nvim-cmp.
+    --  Generally you don't need this, because nvim-cmp will display
+    --  completions whenever it has completion options available.
+    ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       -- select = true,
     })
   }),
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'path' },
+    { name = 'codeium' },
   },
 })
 
