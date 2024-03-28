ab91c56533e26f1cd0e9fe5fab63fcdd3ea22a0f
Matt Warner <mlwarner632@pm.me>
2024-03-28 01:44:24 -0700

    fix: use explicit paths for wiki management

--- neovim/.config/nvim/lua/plugins/wiki.lua
+++ neovim/.config/nvim/lua/plugins/wiki.lua
@@ -6,36 +6,47 @@ return {
         },
         config = function()
             local sep = require("plenary.path").path.sep
-            -- require('kiwi').setup({
-            --     {
-            --         name = 'personal',
-            --         path = '/home/matt/vimwiki',
-            --         -- path = '~/Documents/my_notes'
-            --         -- path = '/Users/mwarner/Library/Mobile Documents/iCloud~md~obsidian/Documents/my_notes'
-            --         -- path = '/Users/mwarner/Library/CloudStorage/ProtonDrive-warnmat@proton.me/my_notes'
-            --         -- path = '~/vimwiki',
-            --     }
-            -- })
+            -- local wikiPath = '~/Documents/my_notes'
+            -- local wikiPath = '/Users/mwarner/Library/CloudStorage/ProtonDrive-warnmat@proton.me/my_notes'
+            -- local wikiPath = '~/vimwiki',
+            -- local wikiPath = '/Users/mwarner/wiki'
+            local wikiPath = '/Users/mwarner/Library/Mobile Documents/iCloud~md~obsidian/Documents/my_notes'
+            local journalPath = wikiPath .. sep .. 'journal'
+
+            require('kiwi').setup({
+                {
+                    name = 'personal',
+                    path = wikiPath,
+                },
+                {
+                    name = 'journal',
+                    path = journalPath
+                }
+            })
 
             local kiwi = require('kiwi')
 
-            local open_daily_journal = function ()
+            local open_daily_journal = function()
                 local isoDate = os.date("%Y-%m-%d")
-                local filepath = kiwi.utils.get_wiki_path() .. sep .. "journal" .. sep .. isoDate .. ".md"
+                local filepath = journalPath .. sep .. isoDate .. ".md"
                 local buffer_number = vim.fn.bufnr(filepath, true)
                 vim.api.nvim_win_set_buf(0, buffer_number)
             end
 
-            vim.keymap.set('n', '<leader>ww', kiwi.open_wiki_index, {})
-            vim.keymap.set('n', '<leader>wd', open_daily_journal, {})
-            vim.keymap.set('n', '<leader>tt', kiwi.todo.toggle, { desc = '[T]oggle [T]ask'})
+            vim.keymap.set('n', '<leader>ww', function() kiwi.open_wiki_index() end, { desc = 'Open [W]iki index' })
+            vim.keymap.set('n', '<leader>wp', function() kiwi.open_wiki_index('personal') end,
+                { desc = 'Open [W]iki [P]ersonal' })
+            vim.keymap.set('n', '<leader>wj', function() kiwi.open_wiki_index('journal') end,
+                { desc = 'Open [W]iki [J]ournal' })
+            vim.keymap.set('n', '<leader>wd', open_daily_journal, { desc = 'Open [W]iki [D]aily journal' })
+            vim.keymap.set('n', '<leader>tt', kiwi.todo.toggle, { desc = '[T]oggle [T]ask' })
 
-            vim.keymap.set('n', '<leader>wf', function()
-                require('telescope.builtin').find_files({ cwd = kiwi.utils.get_wiki_path() })
-            end, { desc = '[W]iki [F]iles' })
-            vim.keymap.set('n', '<leader>wg', function()
-                require('telescope.builtin').live_grep({ cwd = kiwi.utils.get_wiki_path() })
-            end, { desc = '[W]iki [G]rep' })
+            vim.keymap.set('n', '<leader>wsf', function()
+                require('telescope.builtin').find_files({ cwd = wikiPath })
+            end, { desc = '[W]iki [S]earch [F]iles' })
+            vim.keymap.set('n', '<leader>wsg', function()
+                require('telescope.builtin').live_grep({ cwd = wikiPath })
+            end, { desc = '[W]iki [S]earch by [G]rep' })
         end,
     }
 }
