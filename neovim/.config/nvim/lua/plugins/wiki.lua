ef4ef649f632e0e52598e62cc3772790d27a392a
Matt Warner <mlwarner632@pm.me>
2024-11-01 01:44:55 -0700

    feat(nvim): Drop kiwi in favor of native vim functions

--- neovim/.config/nvim/lua/plugins/wiki.lua
+++ neovim/.config/nvim/lua/plugins/wiki.lua
@@ -1,53 +1,35 @@
 return {
-    {
-        'serenevoid/kiwi.nvim',
-        dependencies = {
-            "nvim-lua/plenary.nvim"
-        },
-        config = function()
-            local sep = require("plenary.path").path.sep
-            -- local wikiPath = '~/Documents/my_notes'
-            -- local wikiPath = '/Users/mwarner/Library/CloudStorage/ProtonDrive-warnmat@proton.me/my_notes'
-            -- local wikiPath = '~/vimwiki',
-            -- local wikiPath = '/Users/mwarner/wiki'
-            local wikiPath = '/Users/mwarner/Library/Mobile Documents/iCloud~md~obsidian/Documents/my_notes'
-            -- local wikiPath = '/home/matt/wiki'
-            local journalPath = wikiPath .. sep .. 'journal'
+    'opdavies/toggle-checkbox.nvim',
+    config = function()
+        -- local wikiPath = '~/Documents/my_notes'
+        -- local wikiPath = '/Users/mwarner/Library/CloudStorage/ProtonDrive-warnmat@proton.me/my_notes'
+        -- local wikiPath = '~/vimwiki',
+        -- local wikiPath = '/Users/mwarner/wiki'
+        -- local wikiPath = '/home/matt/wiki'
+        local wikiPath = vim.fs.normalize(
+        '/Users/mwarner/Library/Mobile Documents/iCloud~md~obsidian/Documents/my_notes')
 
-            require('kiwi').setup({
-                {
-                    name = 'personal',
-                    path = wikiPath,
-                },
-                {
-                    name = 'journal',
-                    path = journalPath
-                }
-            })
+        local open_wiki_index = function()
+            local wikiIndexPath = vim.fs.joinpath(wikiPath, 'index.md')
+            vim.cmd.edit(wikiIndexPath)
+        end
 
-            local kiwi = require('kiwi')
+        local open_daily_journal = function()
+            local journalPath = vim.fs.joinpath(wikiPath, 'journal')
+            local isoDate = os.date("%Y-%m-%d")
+            local dailyJournalPath = vim.fs.joinpath(journalPath, isoDate .. '.md')
+            vim.cmd.edit(dailyJournalPath)
+        end
 
-            local open_daily_journal = function()
-                local isoDate = os.date("%Y-%m-%d")
-                local filepath = journalPath .. sep .. isoDate .. ".md"
-                local buffer_number = vim.fn.bufnr(filepath, true)
-                vim.api.nvim_win_set_buf(0, buffer_number)
-            end
+        vim.keymap.set('n', '<leader>ww', open_wiki_index, { desc = 'Open [W]iki index' })
+        vim.keymap.set('n', '<leader>wd', open_daily_journal, { desc = 'Open [W]iki [D]aily journal' })
+        vim.keymap.set("n", "<leader>tt", ":lua require('toggle-checkbox').toggle()<CR>")
 
-            vim.keymap.set('n', '<leader>ww', function() kiwi.open_wiki_index() end, { desc = 'Open [W]iki index' })
-            vim.keymap.set('n', '<leader>wp', function() kiwi.open_wiki_index('personal') end,
-                { desc = 'Open [W]iki [P]ersonal' })
-            vim.keymap.set('n', '<leader>wj', function() kiwi.open_wiki_index('journal') end,
-                { desc = 'Open [W]iki [J]ournal' })
-            vim.keymap.set('n', '<leader>wd', open_daily_journal, { desc = 'Open [W]iki [D]aily journal' })
-            vim.keymap.set('n', '<leader>tt', kiwi.todo.toggle, { desc = '[T]oggle [T]ask' })
-
-            vim.keymap.set('n', '<leader>wsf', function()
-                require('mini.pick').builtin.files({}, { source = { cwd = wikiPath } })
-            end, { desc = '[W]iki [S]earch [F]iles' })
-            vim.keymap.set('n', '<leader>wsg', function()
-                require('mini.pick').builtin.grep_live({}, { source = { cwd = wikiPath } })
-            end, { desc = '[W]iki [S]earch by [G]rep' })
-        end,
-    }
+        vim.keymap.set('n', '<leader>wsf', function()
+            require('mini.pick').builtin.files({}, { source = { cwd = wikiPath } })
+        end, { desc = '[W]iki [S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>wsg', function()
+            require('mini.pick').builtin.grep_live({}, { source = { cwd = wikiPath } })
+        end, { desc = '[W]iki [S]earch by [G]rep' })
+    end,
 }
