4b5e0568b56099f888e73b2d5c8e5660b6ace36f
Matt Warner <mlwarner632@pm.me>
2024-12-18 01:00:23 -0800

    feat(nvim): use blink-cmp for completion

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -57,68 +57,68 @@ vim.opt.scrolloff     = 10
 --- keymaps for builtin completion
 --- https://gist.github.com/MariaSolOs/2e44a86f569323c478e5a078d0cf98cc
 ---@param keys string
-local function feedkeys(keys)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), 'n', true)
-end
+-- local function feedkeys(keys)
+--     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), 'n', true)
+-- end
 
 ---Is the completion menu open?
-local function pumvisible()
-    return tonumber(vim.fn.pumvisible()) ~= 0
-end
+-- local function pumvisible()
+--     return tonumber(vim.fn.pumvisible()) ~= 0
+-- end
 
 -- Use enter to accept completions.
-vim.keymap.set('i', '<cr>', function()
-    return pumvisible() and '<C-y>' or '<cr>'
-end, { expr = true })
+-- vim.keymap.set('i', '<cr>', function()
+--     return pumvisible() and '<C-y>' or '<cr>'
+-- end, { expr = true })
 
 -- Use slash to dismiss the completion menu.
-vim.keymap.set('i', '/', function()
-    return pumvisible() and '<C-e>' or '/'
-end, { expr = true })
+-- vim.keymap.set('i', '/', function()
+--     return pumvisible() and '<C-e>' or '/'
+-- end, { expr = true })
 
 -- Use <C-n> to navigate to the next completion or:
 -- - Trigger LSP completion.
 -- - If there's no one, fallback to vanilla omnifunc.
-vim.keymap.set('i', '<C-n>', function()
-    if pumvisible() then
-        feedkeys '<C-n>'
-    else
-        if next(vim.lsp.get_clients { bufnr = 0 }) then
-            vim.lsp.completion.trigger()
-        else
-            if vim.bo.omnifunc == '' then
-                feedkeys '<C-x><C-n>'
-            else
-                feedkeys '<C-x><C-o>'
-            end
-        end
-    end
-end, { desc = 'Trigger/select next completion' })
+-- vim.keymap.set('i', '<C-n>', function()
+--     if pumvisible() then
+--         feedkeys '<C-n>'
+--     else
+--         if next(vim.lsp.get_clients { bufnr = 0 }) then
+--             vim.lsp.completion.trigger()
+--         else
+--             if vim.bo.omnifunc == '' then
+--                 feedkeys '<C-x><C-n>'
+--             else
+--                 feedkeys '<C-x><C-o>'
+--             end
+--         end
+--     end
+-- end, { desc = 'Trigger/select next completion' })
 
 -- Buffer completions.
-vim.keymap.set('i', '<C-u>', '<C-x><C-n>', { desc = 'Buffer completions' })
+-- vim.keymap.set('i', '<C-u>', '<C-x><C-n>', { desc = 'Buffer completions' })
 
 -- Use <Tab> to accept a Copilot suggestion, navigate between snippet tabstops,
 -- or select the next completion.
 -- Do something similar with <S-Tab>.
-vim.keymap.set({ 'i', 's' }, '<Tab>', function()
-    if pumvisible() then
-        feedkeys '<C-n>'
-    elseif vim.snippet.active { direction = 1 } then
-        vim.snippet.jump(1)
-    else
-        feedkeys '<Tab>'
-    end
-end, {})
-vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
-    if pumvisible() then
-        feedkeys '<C-p>'
-    elseif vim.snippet.active { direction = -1 } then
-        vim.snippet.jump(-1)
-    else
-        feedkeys '<S-Tab>'
-    end
-end, {})
+-- vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+--     if pumvisible() then
+--         feedkeys '<C-n>'
+--     elseif vim.snippet.active { direction = 1 } then
+--         vim.snippet.jump(1)
+--     else
+--         feedkeys '<Tab>'
+--     end
+-- end, {})
+-- vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
+--     if pumvisible() then
+--         feedkeys '<C-p>'
+--     elseif vim.snippet.active { direction = -1 } then
+--         vim.snippet.jump(-1)
+--     else
+--         feedkeys '<S-Tab>'
+--     end
+-- end, {})
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -257,9 +257,9 @@ require('lazy').setup({
 
             -- Autocompletion and signature help
             -- Prefer built in completion in nvim 0.11+
-            if vim.fn.has('nvim-0.11') == 0 then
-                require('mini.completion').setup()
-            end
+            -- if vim.fn.has('nvim-0.11') == 0 then
+            --     require('mini.completion').setup()
+            -- end
 
             -- Highlight usages of the word under the cursor
             require('mini.cursorword').setup()
--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -10,6 +10,62 @@ return {
             -- lazydev.nvim is a plugin that properly configures LuaLS for editing your Neovim config by
             -- lazily updating your workspace libraries.
             { 'folke/lazydev.nvim', opts = {} },
+
+            {
+                'saghen/blink.cmp',
+                lazy = false, -- lazy loading handled internally
+                -- optional: provides snippets for the snippet source
+                dependencies = 'rafamadriz/friendly-snippets',
+
+                -- use a release tag to download pre-built binaries
+                version = 'v0.*',
+                -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+                -- build = 'cargo build --release',
+                -- If you use nix, you can build from source using latest nightly rust with:
+                -- build = 'nix run .#build-plugin',
+
+                ---@module 'blink.cmp'
+                ---@type blink.cmp.Config
+                opts = {
+                    -- 'default' for mappings similar to built-in completion
+                    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+                    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+                    -- see the "default configuration" section below for full documentation on how to define
+                    -- your own keymap.
+                    keymap = { preset = 'default' },
+
+                    completion = {
+                        -- Controls whether the documentation window will automatically show when selecting a completion item
+                        documentation = { auto_show = true },
+                    },
+
+                    -- experimental signature help support
+                    signature = { enabled = true },
+
+
+                    -- default list of enabled providers defined so that you can extend it
+                    -- elsewhere in your config, without redefining it, via `opts_extend`
+                    sources = {
+                        default = { 'lsp', 'path', 'snippets', 'buffer' },
+                        -- optionally disable cmdline completions
+                        -- cmdline = {},
+                    },
+
+                    appearance = {
+                        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+                        -- Useful for when your theme doesn't support blink.cmp
+                        -- will be removed in a future release
+                        use_nvim_cmp_as_default = true,
+                        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                        -- Adjusts spacing to ensure icons are aligned
+                        nerd_font_variant = 'mono'
+                    },
+
+                },
+                -- allows extending the providers array elsewhere in your config
+                -- without having to redefine it
+                opts_extend = { "sources.default" }
+            },
         },
         config = function()
             --  This function gets run when an LSP attaches to a particular buffer.
@@ -76,17 +132,20 @@ return {
                     --  - I found while using it I didn't mind manual triggers, I just disliked that it didn't consider
                     -- 3. Doesn't show function docs while completing.
                     --  my cursor position.
-                    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
-                        if vim.fn.has('nvim-0.11') then
-                            vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
-                        end
-                    end
+                    -- if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+                    --     if vim.fn.has('nvim-0.11') then
+                    --         vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+                    --     end
+                    -- end
                 end,
             })
 
             -- LSP servers and clients are able to communicate to each other what features they support.
-            --  By default, Neovim doesn't support everything that is in the LSP Specification.
+            --  By default, Neovim doesn't support everything that is in the LSP specification.
+            --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+            --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
             local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
 
             -- Enable the following language servers
             --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -173,8 +232,8 @@ return {
                         -- This handles overriding only values explicitly passed
                         -- by the server configuration above. Useful when disabling
                         -- certain features of an LSP (for example, turning off formatting for tsserver)
-                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities,
-                            server.capabilities or {})
+                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+
                         require('lspconfig')[server_name].setup(server)
                     end,
                 },
