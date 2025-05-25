38ba8da422a41ab76bccf4f06879d06dac9d0897
Matt Warner <mlwarner632@pm.me>
2025-05-25 10:02:01 -0700

    refactor(nvim): move completion to separate file

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -19,27 +19,6 @@ return {
                     },
                 }
             },
-
-            {
-                'saghen/blink.cmp',
-                -- enabled = false,
-                -- dev = true,
-                dependencies = { 'rafamadriz/friendly-snippets', },
-                version = '1.*',
-                opts = {
-                    keymap = { preset = 'default' },
-
-                    completion = {
-                        -- Disable showing for all alphanumeric keywords by default. Prefer LSP specific trigger
-                        -- characters.
-                        -- trigger = { show_on_keyword = false },
-                        -- Controls whether the documentation window will automatically show when selecting a completion item
-                        documentation = { auto_show = true },
-                    },
-
-                    signature = { enabled = true },
-                },
-            },
         },
         config = function()
             local servers = {
