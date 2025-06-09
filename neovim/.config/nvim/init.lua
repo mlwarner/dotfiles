df8e0d2d743d0613e77feee8a2a4ee7aee043070
Matt Warner <mlwarner632@pm.me>
2025-06-09 08:17:12 -0700

    fix(nvim): overlapping keybinds

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -239,10 +239,9 @@ require('lazy').setup({
             { "<leader>,",       function() Snacks.picker.buffers() end,                  desc = "Buffers" },
             { "<leader>/",       function() Snacks.picker.grep() end,                     desc = "Grep" },
             { "<leader>:",       function() Snacks.picker.command_history() end,          desc = "Command History" },
-            { "<leader>n",       function() Snacks.picker.notifications() end,            desc = "Notification History" },
             { "<leader>e",       function() Snacks.explorer() end,                        desc = "File Explorer" },
             -- git
-            { "<leader>gb",      function() Snacks.picker.git_branches() end,             desc = "Git Branches" },
+            { "<leader>gbr",     function() Snacks.picker.git_branches() end,             desc = "Git Branches" },
             { "<leader>gl",      function() Snacks.picker.git_log() end,                  desc = "Git Log" },
             { "<leader>gL",      function() Snacks.picker.git_log_line() end,             desc = "Git Log Line" },
             { "<leader>gs",      function() Snacks.picker.git_status() end,               desc = "Git Status" },
@@ -253,13 +252,14 @@ require('lazy').setup({
             { "<leader>sb",      function() Snacks.picker.lines() end,                    desc = "Buffer Lines" },
             { "<leader>sB",      function() Snacks.picker.grep_buffers() end,             desc = "Grep Open Buffers" },
             { "<leader>sg",      function() Snacks.picker.grep() end,                     desc = "Grep" },
-            { "<leader>sw",      function() Snacks.picker.grep_word() end,                desc = "Visual selection or word",      mode = { "n", "x" } },
+            { "<leader>sw",      function() Snacks.picker.grep_word() end,                desc = "Visual selection or word",            mode = { "n", "x" } },
             -- search
             { "<leader>sd",      function() Snacks.picker.diagnostics() end,              desc = "[S]earch [D]iagnostics" },
             { "<leader>sf",      function() Snacks.picker.files() end,                    desc = "[S]earch [F]iles" },
             { "<leader>sh",      function() Snacks.picker.help() end,                     desc = "[S]earch [H]elp" },
             { "<leader>sk",      function() Snacks.picker.keymaps() end,                  desc = "[S]earch [K]eymaps" },
-            { "<leader>sw",      function() Snacks.picker.grep_word() end,                desc = "[S]earch current [W]ord" },
+            { "<leader>sn",      function() Snacks.picker.notifications() end,            desc = "[S]earch [N]otifications" },
+            { "<leader>sw",      function() Snacks.picker.grep_word() end,                desc = "[S]earch Visual selection or [W]ord", mode = { "n", "x" } },
             { "<leader>sr",      function() Snacks.picker.resume() end,                   desc = "[S]earch [R]esume" },
             -- notes
             { "<leader>nsf",     function() Snacks.picker.files({ cwd = notes_dir }) end, desc = "[N]otes [S]earch [F]iles" },
@@ -267,7 +267,7 @@ require('lazy').setup({
             -- LSP
             { "gO",              function() Snacks.picker.lsp_symbols() end,              desc = "LSP Symbols" },
             { "gW",              function() Snacks.picker.lsp_workspace_symbols() end,    desc = "LSP Workspace Symbols" },
-            { "grr",             function() Snacks.picker.lsp_references() end,           nowait = true,                          desc = "References" },
+            { "grr",             function() Snacks.picker.lsp_references() end,           nowait = true,                                desc = "References" },
             { "gri",             function() Snacks.picker.lsp_implementations() end,      desc = "Goto Implementation" },
             { "grd",             function() Snacks.picker.lsp_definitions() end,          desc = "Goto Definition" },
             { "grD",             function() Snacks.picker.lsp_declarations() end,         desc = "Goto Declaration" },
@@ -278,8 +278,8 @@ require('lazy').setup({
             { "<leader>.",       function() Snacks.scratch() end,                         desc = "Toggle Scratch Buffer" },
             { "<leader>S",       function() Snacks.scratch.select() end,                  desc = "Select Scratch Buffer" },
             { "<leader>cR",      function() Snacks.rename.rename_file() end,              desc = "Rename File" },
-            { "<leader>gb",      function() Snacks.git.blame_line() end,                  desc = "Git blame",                     mode = { "n", "v" } },
-            { "<leader>gB",      function() Snacks.gitbrowse() end,                       desc = "Git Browse",                    mode = { "n", "v" } },
+            { "<leader>gbl",     function() Snacks.git.blame_line() end,                  desc = "Git blame",                           mode = { "n", "v" } },
+            { "<leader>gB",      function() Snacks.gitbrowse() end,                       desc = "Git Browse",                          mode = { "n", "v" } },
             { "<c-/>",           function() Snacks.terminal() end,                        desc = "Toggle Terminal" },
             { "<leader>ps",      function() Snacks.profiler.scratch() end,                desc = "Profiler Scratch Bufer" },
             { "<leader>pp",      function() Snacks.profiler.toggle() end,                 desc = "Toggle the profiler" },
@@ -387,7 +387,7 @@ require('lazy').setup({
             -- })
 
             -- Miscellaneous useful functions
-            require('mini.misc')
+            -- require('mini.misc')
 
             -- Move any selection in any direction
             require('mini.move').setup()
