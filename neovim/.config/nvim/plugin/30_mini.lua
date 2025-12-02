ad8de5a4fcbc4f72d3cd141403e43091569079b4
Matt Warner <mlwarner632@pm.me>
2025-12-01 20:10:31 -0800

    feat(nvim): add clues for toggle buttons

--- neovim/.config/nvim/plugin/30_mini.lua
+++ neovim/.config/nvim/plugin/30_mini.lua
@@ -59,21 +59,21 @@ end)
 --
 -- Uses `now()` for `setup_xxx()` to work when started like `nvim -- path/to/file`
 now_if_args(function()
-  -- Makes `:h MiniMisc.put()` and `:h MiniMisc.put_text()` public
-  require('mini.misc').setup()
+    -- Makes `:h MiniMisc.put()` and `:h MiniMisc.put_text()` public
+    require('mini.misc').setup()
 
-  -- Change current working directory based on the current file path. It
-  -- searches up the file tree until the first root marker ('.git' or 'Makefile')
-  -- and sets their parent directory as a current directory.
-  -- This is helpful when simultaneously dealing with files from several projects.
-  MiniMisc.setup_auto_root()
+    -- Change current working directory based on the current file path. It
+    -- searches up the file tree until the first root marker ('.git' or 'Makefile')
+    -- and sets their parent directory as a current directory.
+    -- This is helpful when simultaneously dealing with files from several projects.
+    MiniMisc.setup_auto_root()
 
-  -- Restore latest cursor position on file open
-  MiniMisc.setup_restore_cursor()
+    -- Restore latest cursor position on file open
+    MiniMisc.setup_restore_cursor()
 
-  -- Synchronize terminal emulator background with Neovim's background to remove
-  -- possibly different color padding around Neovim instance
-  MiniMisc.setup_termbg_sync()
+    -- Synchronize terminal emulator background with Neovim's background to remove
+    -- possibly different color padding around Neovim instance
+    MiniMisc.setup_termbg_sync()
 end)
 
 now(function() require('mini.notify').setup() end)
@@ -145,16 +145,23 @@ later(function()
     require('mini.bufremove').setup()
 end)
 
+-- Show next key clues in a bottom right window. Requires explicit opt-in for
+-- keys that act as clue trigger. Example usage:
+-- - Press `<Leader>` and wait for 1 second. A window with information about
+--   next available keys should appear.
+-- - Press one of the listed keys. Window updates immediately to show information
+--   about new next available keys. You can press `<BS>` to go back in key sequence.
+-- - Press keys until they resolve into some mapping.
+--
+-- Note: it is designed to work in buffers for normal files. It doesn't work in
+-- special buffers (like for 'mini.starter' or 'mini.files') to not conflict
+-- with its local mappings.
+--
+-- See also:
+-- - `:h MiniClue-examples` - examples of common setups
+-- - `:h MiniClue.ensure_buf_triggers()` - use it to enable triggers in buffer
+-- - `:h MiniClue.set_mapping_desc()` - change mapping description not from config
 later(function()
-    -- Show next key clues. Example usage:
-    -- - Press `<Leader>` and wait for 1 second. A window with information about
-    --   next available keys should appear.
-    -- - Press one of the listed keys. Window updates immediately to show information
-    --   about new next available keys. You can press `<BS>` to go back in key sequence.
-    -- - Press keys until they resolve into some mapping.
-    --
-    -- See also:
-    -- - `:h MiniClue-examples` - examples of common setups
     local miniclue = require('mini.clue')
     -- stylua: ignore
     miniclue.setup({
@@ -167,35 +174,37 @@ later(function()
             miniclue.gen_clues.g(),
             miniclue.gen_clues.marks(),
             miniclue.gen_clues.registers(),
-            miniclue.gen_clues.windows(),
+            -- This creates a submode for window resize mappings. Try the following:
+            -- - Press `<C-w>s` to make a window split.
+            -- - Press `<C-w>+` to increase height. Clue window still shows clues as if
+            --   `<C-w>` is pressed again. Keep pressing just `+` to increase height.
+            --   Try pressing `-` to decrease height.
+            -- - Stop submode either by `<Esc>` or by any key that is not in submode.
+            miniclue.gen_clues.windows({ submode_resize = true }),
             miniclue.gen_clues.z(),
-            -- mini.bracketed provides `[` and `]` mappings
-            { mode = 'n', keys = '[b', desc = 'Previous buffer' },
-            { mode = 'n', keys = ']b', desc = 'Next buffer' },
-            { mode = 'n', keys = '[d', desc = 'Previous diagnostic' },
-            { mode = 'n', keys = ']d', desc = 'Next diagnostic' },
         },
         -- Explicitly opt-in for set of common keys to trigger clue window
         triggers = {
             { mode = 'n', keys = '<Leader>' }, -- Leader triggers
             { mode = 'x', keys = '<Leader>' },
-            { mode = 'n', keys = '[' },        -- mini.bracketed
+            { mode = 'n', keys = '\\' }, -- mini.basics
+            { mode = 'n', keys = '[' },  -- mini.bracketed
             { mode = 'n', keys = ']' },
             { mode = 'x', keys = '[' },
             { mode = 'x', keys = ']' },
-            { mode = 'i', keys = '<C-x>' },    -- Built-in completion
-            { mode = 'n', keys = 'g' },        -- `g` key
+            { mode = 'i', keys = '<C-x>' }, -- Built-in completion
+            { mode = 'n', keys = 'g' }, -- `g` key
             { mode = 'x', keys = 'g' },
-            { mode = 'n', keys = "'" },        -- Marks
+            { mode = 'n', keys = "'" }, -- Marks
             { mode = 'n', keys = '`' },
             { mode = 'x', keys = "'" },
             { mode = 'x', keys = '`' },
-            { mode = 'n', keys = '"' },        -- Registers
+            { mode = 'n', keys = '"' }, -- Registers
             { mode = 'x', keys = '"' },
             { mode = 'i', keys = '<C-r>' },
             { mode = 'c', keys = '<C-r>' },
-            { mode = 'n', keys = '<C-w>' },    -- Window commands
-            { mode = 'n', keys = 'z' },        -- `z` key
+            { mode = 'n', keys = '<C-w>' }, -- Window commands
+            { mode = 'n', keys = 'z' }, -- `z` key
             { mode = 'x', keys = 'z' },
         },
     })
